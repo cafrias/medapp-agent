773ee3511c7a262753119278b1d407121b66950f
Carlos Frias <carlos.a.frias@gmail.com>
2025-05-31 01:53:52 -0300

    feat: include MCP server to API

--- api/src/main.py
+++ api/src/main.py
@@ -1,10 +1,11 @@
+from fastapi_mcp import FastApiMCP
 import uvicorn
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from config import Database
 from models import Professional, Slot
 from fastapi import HTTPException
-from typing import List
+from typing import List, Optional
 from bson.objectid import ObjectId
 from datetime import datetime
 
@@ -16,11 +17,11 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-@app.get("/professionals/{professional_id}/slots", response_model=List[Slot])
+@app.get("/professionals/{professional_id}/slots", response_model=List[Slot], operation_id="get_time_slots")
 async def get_professional_slots(
     professional_id: str,
-    end: str, 
-    start: str,
+    end: Optional[str] = None, 
+    start: Optional[str] = None,
 ):
     db = Database.get_db()
     professional = await Professional.get_by_id(db, professional_id)
@@ -28,9 +29,9 @@ async def get_professional_slots(
         raise HTTPException(status_code=404, detail="Professional not found")
     
     
-    start = datetime.now().isoformat() if start == "" else start
+    start = datetime.now().isoformat() if not start else start
     time_filter = { "$gte": start }
-    if end != "":
+    if end:
         time_filter["$lte"] = end
 
     slots = await Slot.get_many_by_query(db, {
@@ -41,6 +42,8 @@ async def get_professional_slots(
 
     return slots
 
+mcp = FastApiMCP(app, include_operations=["get_time_slots"])
+mcp.mount()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
